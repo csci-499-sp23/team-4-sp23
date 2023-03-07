83007fb24cc4527c0d15f6a77b32bbb98ca6282b
Justin Salle <justinsalle68@gmail.com>
2023-03-06 23:23:46 -0500

    main

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <a className="NavButtons" href="https://reactjs.org">Logo</a>
+            <a className="NavButtons" href="https://reactjs.org">About</a>
+            <a className="NavButtons" href="https://reactjs.org">Contact Us</a>
+            <a className="NavButtons" href="https://reactjs.org">Log In</a>
+            <a className="NavButtons" href="https://reactjs.org">Sign Up</a>
+        </div>
+    )
+}
