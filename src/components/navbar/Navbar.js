9272326d155cddcd62c6aab65fc640af114cbd4a
Justin Salle <justinsalle68@gmail.com>
2023-03-19 12:12:28 -0400

    Changed styling and added content to about page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar= () =>{
     return(
     <div className ="Navbar">
-        <div className = "NavButtons">
-            <Link to='/home'><img src= {require('./NavbarLogo.png')} alt='Logo' className = 'NavLogo'></img></Link>
+        <div>
+            <Link to='/home'><img src= {require('../img/NavbarLogo.png')} alt='Logo' className = 'NavLogo'></img></Link>
         </div>
-        <div className = "NavButtons">
+        <div>
             <Link to='/about'className = "NavButtons">About</Link>
         </div>
-        <div className = "NavButtons">
+        <div>
             <Link to='/contact' className = "NavButtons">Contact Us</Link>
         </div>
-        <div className = "NavButtons">
+        <div>
             <Link to= '/login' className = "NavButtons">Login</Link>
         </div>
-        <div className = "NavButtons">
+        <div>
             <Link to= '/signUp' className = "NavButtons">Sign Up</Link>
         </div>
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import sitelogo from './SiteLogo.png';
+import sitelogo from '../img/SiteLogo.png';
 const Home = () =>{
     return(
     <div>
-        <header className="App-header">
-        <img src= {sitelogo} alt="logo" className = "SiteLogo"/>
-        <p>
-          Welcome to Wheel Call You, where we make your moving experience that much better!
-        </p>
-      </header>
+        <div className='Home-top'>
+          <h1>Welcome to Wheel Call You, where we make your moving experience that much better!</h1>
+          <img src= {sitelogo} alt="logo" className = "SiteLogo"/>
+        </div>
+        
     </div>
     );
 };
