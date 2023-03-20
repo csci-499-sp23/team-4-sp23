49026636050012e0291ac5ae6f600c2ed24865bb
Joseph Planer <josephplaner@Josephs-MacBook-Pro.local>
2023-03-19 22:24:40 -0400

    updated sign up page to connect to firebase authentication

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { createUserWithEmailAndPassword } from '@firebase/auth'
+import { auth } from '../../firebase-config'
+
 const SignUp = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signUp = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            console.log(userCredential);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
     return(
         <div>
         <h1>Sign Up</h1>
-
-        <form action='/signUp.php' method="get">
-            <label for="fName">First Name</label>
-            <br></br>
-            <input type="text" id="fName" name="fName"></input>
-            <br></br>
-            <label for="lName">Last Name</label>
-            <br></br>
-            <input type="text" id="lName" name="lName"></input>
-            <br></br>
+        <form onSubmit = {signUp}>
             <label for="email">Email Address</label>
             <br></br>
-            <input type="text" id="email" name="email"></input>
+            <input 
+                type="email" 
+                placeholder="Enter your email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}>
+            </input>
             <br></br>
-            <label for="username">Username</label>
+            <label for="email">Password</label>
             <br></br>
-            <input type="text" id="username" name="username"></input>
+            <input 
+                type="password" 
+                placeholder="Enter your password" 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}>
+            </input>
             <br></br>
-            <label for="password">Password</label>
             <br></br>
-            <input type="password" id="password" name="password"></input>
-            <br></br>
-            <label for="password">Confirm Password</label>
-            <br></br>
-            <input type="password" id="password" name="password"></input>
-            <br></br><br></br>
-            <input type="submit" value="Submit"></input>
+            <button type="submit">Sign Up</button>
         </form>
     </div>
     );
