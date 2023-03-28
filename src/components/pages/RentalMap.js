65ab281926ea005db47163fdb68d10e2aaf8c52c
spiiral <alex.okeefe14@myhunter.cuny.edu>
2023-03-28 14:49:16 -0400

    big refactor

--- src/components/pages/RentalMap.js
+++ src/components/pages/RentalMap.js
@@ -1,130 +1,75 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, InfoWindow, LoadScript, Marker } from "@react-google-maps/api";
 import { db } from '../../firebase-config.js';
-import { collection, query, where, getDocs, get } from 'firebase/firestore';
+import { collection, getDocs } from 'firebase/firestore';
 import Geocode from 'react-geocode';
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
-Geocode.setApiKey('AIzaSyDhz3m22jJJjC6BOX83Qbjdm2FaQiXVK4A')
+Geocode.setApiKey('AIzaSyDhz3m22jJJjC6BOX83Qbjdm2FaQiXVK4A');
+
 const RentalMap = () => {
-  class Location {
-    constructor(street, city, state, zip, link, location) {
-      this.street = street;
-      this.city = city;
-      this.state = state;
-      this.zip = zip;
-      this.link = link;
-      this.location = location;
-    }
-    
-    getFullAddress() {
-      return `${this.street}, ${this.city}, ${this.state} ${this.zip}`;
-    }
-  };
-  const initialMarkers = [];
-
   const [activeInfoWindow, setActiveInfoWindow] = useState(null);
-  const [markers, setMarkers] = useState(initialMarkers);
+  const [markers, setMarkers] = useState([]);
 
-  const containerStyle = {
-    width: '100%',
-    height: '900px',
-  };
+  const containerStyle = { width: '100%', height: '900px' };
+  const center = { lat: 40.7678, lng: -73.9645 };
 
-  const center = {
-    lat: 40.7678,
-    lng: -73.9645,
-  };
-
-  const mapClicked = (event) => {
-    console.log(event.latLng.lat(), event.latLng.lng())
-  };
-
-  const markerClicked = (marker) => {
-    setActiveInfoWindow(marker);
-  };
-
-  const markerDragEnd = (event, index) => {
-    console.log(event.latLng.lat())
-    console.log(event.latLng.lng())
-  };
+  const handleInfoWindowClose = () => setActiveInfoWindow(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const dLocations = collection(db, "trucks");
-      const querySnapshot = await getDocs(dLocations);
-    
-      querySnapshot.forEach((doc) => {
-        const { addresses, links, locations } = doc.data();
-        const addressComponents = addresses.split(" ");
-        const zip = addressComponents.pop();
-        const state = addressComponents.pop();
-        const city = addressComponents.pop();
-        const street = addressComponents.join(" ");
-    
-        const locationObject = new Location(street, city, state, zip, links, locations);
-    
-        Geocode.fromAddress(locationObject.getFullAddress()).then(
-          (response) => {
-            const { lat, lng } = response.results[0].geometry.location;
-            const marker = {
-              position: {
-                lat,
-                lng,
-              },
-              label: {
-                color: "black",
-                text: " ",
-              },
-              link: locationObject.link,
-              location: locationObject.location,
-            };
-            setMarkers((prevMarkers) => [...prevMarkers, marker]);
-          },
-          (error) => {
-            console.error(error);
-          }
-        );
-      });
+      const querySnapshot = await getDocs(collection(db, 'trucks'));
+
+      const newMarkers = await Promise.all(
+        querySnapshot.docs.map(async (doc) => {
+          const { addresses, links, locations } = doc.data();
+          const addressComponents = addresses.split(' ');
+          const zip = addressComponents.pop();
+          const state = addressComponents.pop();
+          const city = addressComponents.pop();
+          const street = addressComponents.join(' ');
+
+          const location = `${street}, ${city}, ${state} ${zip}`;
+
+          const geocodeResponse = await Geocode.fromAddress(location);
+          const { lat, lng } = geocodeResponse.results[0].geometry.location;
+
+          return {
+            position: { lat, lng },
+            label: { color: 'black', text: ' ' },
+            link: links,
+            location,
+          };
+        })
+      );
+
+      setMarkers(newMarkers);
     };
-    
+
     fetchData();
   }, []);
-  
-  const handleInfoWindowClose = () => {
-    setActiveInfoWindow(null);
-  };
 
   return (
     <LoadScript googleMapsApiKey='AIzaSyDhz3m22jJJjC6BOX83Qbjdm2FaQiXVK4A'>
-      <GoogleMap 
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={15}
-        onClick={mapClicked}
-      >
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
         {markers.map((marker, index) => (
           <Marker
             key={index}
             position={marker.position}
             label={marker.label}
+            icon={require('../img/marker.png')}
             draggable={false}
-            onClick={() => markerClicked(marker)}
+            onClick={() => setActiveInfoWindow(marker)}
           >
             {activeInfoWindow === marker && (
               <InfoWindow
                 position={marker.position}
                 onCloseClick={handleInfoWindowClose}
-                style={{ text: 'black' }}
+                style={{ color: 'black' }}
               >
                 <div>
-                  <p>
-                    <strong>Location: </strong>
-                    {marker.location}
-                  </p>
-                  <p>
-                  <strong>Link: </strong>
-                  <a href={marker.link}>{marker.link}</a>
-                  </p>
+                  <p><strong>Location: </strong>{marker.location}</p>
+                  <p><strong>Link: </strong><a href={marker.link}>{marker.link}</a></p>
                 </div>
               </InfoWindow>
             )}
