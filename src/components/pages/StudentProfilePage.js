a00796152c96662a7dec79541d6549ec7d30ebfe
Joseph Planer <josephplaner@josephs-mbp.myfiosgateway.com>
2023-05-15 20:45:48 -0400

    Updated profile page so field values are retained when clicking edit, with an additional function to clear the fields using a separate button if needed

--- src/components/pages/StudentProfilePage.js
+++ src/components/pages/StudentProfilePage.js
@@ -10,6 +10,7 @@ import { faEdit, faCheckCircle } from '@fortawesome/fontawesome-free-solid'
 
 const StudentProfilePage = () => {
   const user = useUserSelector();
+  const [firstName, setFirstName] = useState("");
   const [bio, setBio] = useState("");
   const [location, setLocation] = useState("");
   const [USState, setUSState] = useState("");
@@ -22,6 +23,10 @@ const StudentProfilePage = () => {
   const [userImage, setUserImage] = useState("https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=");
   const [studentData, setStudentData] = useState(null);
   const [imageUpload, setImageUpload] = useState(null);
+  const [editBio, setEditBio] = useState(false);
+  const [editAddress, setEditAddress] = useState(false);
+  const [editDOB, setEditDOB] = useState(false);
+  const [editUniversity, setEditUniversity] = useState(false);
  
   useEffect(() => {
     const fetchSchools = async () => {
@@ -50,6 +55,7 @@ const StudentProfilePage = () => {
         const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
         setStudentData(data);
         if (data.length > 0) {
+          setFirstName(data[0].first_name);
           setLocation(data[0].street_add);
           setDOB(data[0].dob);
           setUniversityID(data[0].university);
@@ -84,12 +90,10 @@ const StudentProfilePage = () => {
     const imageRef = ref(storage, imageName)
   
     uploadBytes(imageRef, imageUpload).then(async () => {
-      // get the download URL after the upload is complete
       const url = await getDownloadURL(imageRef);
       
       const studentDoc = doc(db, "students", studentData[0].id);
       
-      // save the URL to Firestore
       await updateDoc(studentDoc, { image: url });
   
       alert("Image Uploaded");
@@ -98,12 +102,29 @@ const StudentProfilePage = () => {
 
   
   const displayBio = () => {
-    if (user?.email && studentData && studentData[0].bio !== undefined && studentData[0].bio !== "") {
+    if (editBio) {
+      return (
+        <div className="d-flex flex-column">
+          <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="5" name="issue" value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
+          <div className="d-flex justify-content-end">
+            <p className="x-clear" onClick={clearBio}><strong>X</strong></p>
+            <div className="icon-wrapper" onClick={updateBio}>
+              <FontAwesomeIcon
+                className="icon-pointer"
+                icon={faCheckCircle}
+                size="lg"
+                style={{ color: "#38ab07", cursor: "pointer" }}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    } else if (user?.email && studentData && studentData[0].bio !== undefined && studentData[0].bio !== "") {
       return (
         <div className="d-flex flex-column">
           <div className="bio-text">{studentData[0].bio}</div>
           <div className="d-flex justify-content-end">
-            <div className="icon-wrapper" onClick={clearBio}>
+            <div className="icon-wrapper" onClick={() => setEditBio(true)}>
               <FontAwesomeIcon
                 className="icon-pointer"
                 icon={faEdit}
@@ -117,7 +138,8 @@ const StudentProfilePage = () => {
     } else {
       return (
         <div className="d-flex flex-column">
-          <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="5" name="issue" onChange={(e) => setBio(e.target.value)}></textarea>
+          <p className="card-text">Tell us a little more about yourself.</p>
+          <textarea className="form-control mb-3" id="exampleFormControlTextarea1" rows="5" name="issue" value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
           <div className="d-flex justify-content-end">
             <div className="icon-wrapper" onClick={updateBio}>
               <FontAwesomeIcon
@@ -134,6 +156,7 @@ const StudentProfilePage = () => {
   };
 
   const updateBio = async () => {
+    setEditBio(false);
     const studentDoc = doc(db, "students", studentData[0].id);
     if (bio !== "") {
       await updateDoc(studentDoc, { bio: bio });
@@ -143,6 +166,7 @@ const StudentProfilePage = () => {
   const clearBio = async () => {
     const studentDoc = doc(db, "students", studentData[0].id);
     await updateDoc(studentDoc, { bio: "" });
+    setEditBio(false);
   };
 
   const displayImage = () => {
@@ -195,7 +219,59 @@ const StudentProfilePage = () => {
 
   
   const displayStreetAddress = () => {
-    if (
+    if (editAddress) {
+      return (
+        <div className="d-flex flex-column">
+          <label>Street Address</label>
+          <input
+            required
+            type="text"
+            className="form-control input-field mb-3"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
+          <label>State</label>
+          <select
+            className="form-select input-field mb-3"
+            value={USState}
+            onChange={(e) => setUSState(e.target.value)}
+          >
+            <option selected>Please select your state</option>
+            <option value="MA">MA</option>
+            <option value="NJ">NJ</option>
+            <option value="NY">NY</option>
+          </select>
+          <label>City</label>
+          <input
+            required
+            type="text"
+            className="form-control input-field mb-3"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          ></input>
+          <label>ZIP</label>
+          <input
+            required
+            type="text"
+            className="form-control input-field mb-3"
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          ></input>
+          <div className="d-flex justify-content-end">
+            <p className="x-clear" onClick={clearAddress}><strong>X</strong></p>
+            <div className="icon-wrapper" onClick={updateStreetAddress}>
+              <FontAwesomeIcon
+                className="icon-pointer"
+                icon={faCheckCircle}
+                size="lg"
+                style={{ color: "#38ab07", cursor: "pointer" }}
+              />
+            </div>
+            
+          </div>
+        </div>
+      );
+    } else if (
       user?.email &&
       studentData &&
       studentData[0].street_add !== undefined &&
@@ -222,7 +298,7 @@ const StudentProfilePage = () => {
             <div>{studentData[0].zip}</div>
           </div>
           <div className="d-flex justify-content-end">
-            <div className="icon-wrapper" onClick={clearAddress}>
+            <div className="icon-wrapper" onClick={() => setEditAddress(true)}>
               <FontAwesomeIcon
                 className="icon-pointer"
                 icon={faEdit}
@@ -239,7 +315,7 @@ const StudentProfilePage = () => {
           <label>Street Address</label>
           <input required type="text" className="form-control input-field mb-3" onChange={(e) => setLocation(e.target.value)}></input>
           <label>State</label>
-          <select class="form-select input-field mb-3" aria-label="Default select example" onChange={(e) => setUSState(e.target.value)}>
+          <select class="form-select input-field mb-3" onChange={(e) => setUSState(e.target.value)}>
             <option selected>Please select your state</option>
             <option value="MA">MA</option>
             <option value="NJ">NJ</option>
@@ -267,12 +343,36 @@ const StudentProfilePage = () => {
   
 
   const displayDOB = () => {
-    if (user?.email && studentData && studentData[0].dob !== undefined && studentData[0].dob !== "") {
+    if (editDOB) {
+      return (
+        <div className="d-flex flex-column">
+          <input 
+            required 
+            type="date" 
+            className="form-control input-field" 
+            value={dob}
+            onChange={(e) => setDOB(e.target.value)}
+          ></input>
+          <div className="d-flex justify-content-end">
+            <p className="x-clear" onClick={clearDOB}><strong>X</strong></p>
+            <div className="icon-wrapper" onClick={() => {updateDOB()}}>
+              <FontAwesomeIcon
+                className="icon-pointer"
+                icon={faCheckCircle}
+                size="lg"
+                style={{ color: "#38ab07", cursor: "pointer" }}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    } else if (user?.email && studentData && studentData[0].dob !== undefined && studentData[0].dob !== "") {
       return (
         <div className="d-flex flex-column">
           <div>{studentData[0].dob}</div>
           <div className="d-flex justify-content-end">
-            <div className="icon-wrapper" onClick={clearDOB}>
+          
+            <div className="icon-wrapper" onClick={() => setEditDOB(true)}>
               <FontAwesomeIcon
                 className="icon-pointer"
                 icon={faEdit}
@@ -304,6 +404,7 @@ const StudentProfilePage = () => {
   
 
   const updateStreetAddress = async () => {
+    setEditAddress(false);
     const studentDoc = doc(db, "students", studentData[0].id);
     if (location !== "" && USState !== "" && city !== "" && zip !== "") {
       await updateDoc(studentDoc, { street_add: location });
@@ -324,9 +425,11 @@ const StudentProfilePage = () => {
     setUSState("");
     setCity("");
     setZip("");
+    setEditAddress(false);
   };
 
   const updateDOB = async () => {
+    setEditDOB(false);
     const studentDoc = doc(db, "students", studentData[0].id);
     if (dob !== "") {
       await updateDoc(studentDoc, { dob: dob });
@@ -336,6 +439,7 @@ const StudentProfilePage = () => {
   const clearDOB = async () => {
     const studentDoc = doc(db, "students", studentData[0].id);
     await updateDoc(studentDoc, { dob: "" });
+    setEditDOB(false);
   };
 
   const setSelectedUniversity = (id, name) => {
@@ -344,7 +448,42 @@ const StudentProfilePage = () => {
   };
   
   const displayUniversity = () => {
-    if (
+    if (editUniversity) {
+      return (
+        <div className="d-flex flex-column">
+          <div className="position-relative">
+            <div className="university-wrapper">
+              <select
+                className="form-select input-field"
+                value={universityID} 
+                onChange={(e) => {
+                  const selectedSchool = schoolsList.find((school) => school.id === e.target.value);
+                  setSelectedUniversity(selectedSchool.id, selectedSchool.name);
+                }}
+              >
+                <option>Please select your university</option>
+                {schoolsList.map((school) => (
+                  <option key={school.id} value={school.id}>
+                    {school.name}
+                  </option>
+                ))}
+              </select>
+              <div className="d-flex justify-content-end">
+                <p className="x-clear" onClick={clearUniversity}><strong>X</strong></p>
+                <div className="edit-icon-wrapper" onClick={() => {updateUniversity()}}>
+                  <FontAwesomeIcon
+                    className="icon-pointer"
+                    icon={faCheckCircle}
+                    size="lg"
+                    style={{ color: "#38ab07", cursor: "pointer" }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (
       user?.email &&
       studentData &&
       studentData[0].university !== undefined &&
@@ -354,7 +493,7 @@ const StudentProfilePage = () => {
         <div className="d-flex flex-column">
           <div>{university}</div>
           <div className="d-flex justify-content-end">
-            <div className="icon-wrapper" onClick={clearUniversity}>
+            <div className="icon-wrapper" onClick={() => setEditUniversity(true)}>
               <FontAwesomeIcon
                 className="icon-pointer"
                 icon={faEdit}
@@ -372,7 +511,6 @@ const StudentProfilePage = () => {
             <div className="university-wrapper">
               <select
                 className="form-select input-field"
-                aria-label="Default select example"
                 onChange={(e) => {
                   const selectedSchool = schoolsList.find((school) => school.id === e.target.value);
                   setSelectedUniversity(selectedSchool.id, selectedSchool.name);
@@ -403,6 +541,7 @@ const StudentProfilePage = () => {
   };  
 
   const updateUniversity = async () => {
+    setEditUniversity(false);
     const studentDoc = doc(db, "students", studentData[0].id);
     if (university !== "") {
       await updateDoc(studentDoc, { university:  universityID });
@@ -412,6 +551,7 @@ const StudentProfilePage = () => {
   const clearUniversity = async () => {
     const studentDoc = doc(db, "students", studentData[0].id);
     await updateDoc(studentDoc, { university: "" });
+    setEditUniversity(false);
   };
   
 
@@ -419,11 +559,10 @@ const StudentProfilePage = () => {
     <div className="row row-cols-1 row-cols-md-2 g-4">
       <div className="col">
         <div className="card">
+          <h2>Hi, {firstName}!</h2>
           {displayImage()}
           <div className="card-body">
             <h5 className="card-title">Bio</h5>
-            <p className="card-text">Tell us a little more about yourself.</p>
-            {user?.email}
             {displayBio()}
             <br></br>
             <div class="button-container">
